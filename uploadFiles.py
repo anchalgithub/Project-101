9b794cac848cc9a62ea3cfdf162efff7cce77310
ASinha <69646570+anchalgithub@users.noreply.github.com>
2021-03-31 08:47:02 -0400

    Update uploadFiles.py

--- uploadFiles.py
+++ uploadFiles.py
@@ -15,8 +15,8 @@ def main():
     access_token = 'FIlGy3UR5H0AAAAAAAAAATtNeCJYfg1x9cylsBHP7Jut0vmPGSGUBaAYJrZi8TF1'
     transferData = TransferData(access_token)
 
-    file_from = input("Enter the file path to transfer :")
-    file_to = input("enter the full path to upload to dropbox: ")  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
+    file_from = input("Enter the file path to transfer: ")
+    file_to = input("Enter the full path to upload to dropbox: ")  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
 
     # API v2
     transferData.upload_file(file_from, file_to)
